7149a69f6708b8ee3c6b2a9fe518a9ad4c2015d2
Yefri Tavarez <yefritavarez@gmail.com>
2025-03-12 23:08:09 -0400

    feat: Add environment selection and service URL handling to ECF Settings

--- ecf/ecf/doctype/ecf_settings/ecf_settings.js
+++ ecf/ecf/doctype/ecf_settings/ecf_settings.js
@@ -1,8 +1,33 @@
 // Copyright (c) 2025, TzCode, S. R. L. and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("ECF Settings", {
-// 	refresh(frm) {
+{
+	function environment(frm) {
+		// TesteCF: Ambiente de pre-certificación:
+		// https://ecf.dgii.gov.do/testecf/autenticacion
+		//
+		// CerteCF: Ambiente de certificación:
+		// https://ecf.dgii.gov.do/certecf/autenticacion
+		//
+		// eCF: Ambiente de producción:
+		// https://ecf.dgii.gov.do/ecf/autenticacion
+		//
+		
+		const { doc } = frm;
+		if (doc.environment === "TesteCF") {
+			doc.service_url = "https://ecf.dgii.gov.do/testecf/autenticacion";
+		} else if (doc.environment === "CerteCF") {
+			doc.service_url = "https://ecf.dgii.gov.do/certecf/autenticacion";
+		} else if (doc.environment === "eCF") {
+			doc.service_url = "https://ecf.dgii.gov.do/ecf/autenticacion";
+		} else {
+			doc.service_url = "";
+		}
+		
+		frm.refresh_field("service_url");
+	}
 
-// 	},
-// });
+	frappe.ui.form.on("ECF Settings", {
+		environment,
+	});
+}
