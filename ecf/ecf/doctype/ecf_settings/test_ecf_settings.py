1ff04f596c5feda60eb498ee65f08e96bbf9d5f9
Yefri Tavarez <yefritavarez@gmail.com>
2025-03-12 22:36:38 -0400

    feat: Add ECF Settings DocType with initial files and structure

---
+++ ecf/ecf/doctype/ecf_settings/test_ecf_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, TzCode, S. R. L. and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestECFSettings(FrappeTestCase):
+	pass
